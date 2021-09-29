bb8258c62071f5ec0192978c2b52bdbc87615e6a
singhsiddhu <siddhusingh@gmail.com>
2021-09-29 16:04:33 +0800

    changed files

--- src/main/java/com/sid/threading/RunnableAndThread/MTwithThreadClass.java
+++ src/main/java/com/sid/threading/RunnableAndThread/MTwithThreadClass.java
@@ -18,6 +18,7 @@ class DBHandle {
 }
 
 public class MTwithThreadClass extends Thread {
+    // All threads should create on 1 DBHandle
     private static volatile DBHandle dbHandle = null;
 
     public MTwithThreadClass(String name) {
@@ -32,10 +33,12 @@ public class MTwithThreadClass extends Thread {
         System.out.println("dbHanle's name is : " + dbHandle.getMyName());
     }
 
-    public DBHandle getDBHandle() {
-        if (dbHandle == null) { 
+    public static DBHandle getDBHandle() {
+        if (dbHandle == null) {
             synchronized (MTwithThreadClass.class) {
                 if (dbHandle == null) {
+                    // What does this 'new' operator does : i) operator new call ( malloc , memory
+                    // allocation), ii ) constructor call
                     dbHandle = new DBHandle(Thread.currentThread().getName());
                 }
             }
@@ -46,7 +49,7 @@ public class MTwithThreadClass extends Thread {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Main Starts...");
         final int SIZE = 10;
-        
+        // If DBHandle is instantiated here. it will be Single object...
         MTwithThreadClass arrayOfThread[] = new MTwithThreadClass[SIZE];
         for (int i = 0; i < SIZE; ++i) {
             arrayOfThread[i] = new MTwithThreadClass("#" + i);
--- src/main/java/com/sid/threading/ch2/PubSub.java
+++ src/main/java/com/sid/threading/ch2/PubSub.java
@@ -13,7 +13,14 @@ public class PubSub {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello Java World!");
         PubSub pubSub = new PubSub();
-        
+        // Home-Work : Change type of message from int to BIG STRING
+        // Pubslisher should publish 10 different BIG STRINGS
+        // Consumer should consumer each String as a message and do some search operation.
+        // Now calcualte time at the end of main function
+        // II case : 1 Pub multiple Sub : check the performance issues. 
+        // HINT : have as many subs as CPUs in your computer 
+        // a) Have #Cons < #CPU , > , ==
+
         // Functional interfaces. The Runnable interface—like the Callable<T> interface, the Comparator<T> interface, and 
         // a whole host of other interfaces already defined within Java—is what Java 8 calls a functional interface: it 
         // is an interface that requires exactly one method to be implemented in order to satisfy the requirements of the
@@ -24,12 +31,15 @@ public class PubSub {
         // documentation hint that an interface is designed to be used with lambdas, but the compiler does not require 
         // this—it determines “functional interfaceness” from the structure of the interface, not from the annotation.
         // https://github.com/ganfanlaowang/Java8-Source-Code/blob/master/src/main/jdk8/java/lang/Runnable.java
+        
+        // StartTime;
         Thread consumer = new Thread(()->pubSub.subscriber());
         Thread producer = new Thread(()->pubSub.publisher());
         consumer.start();
         producer.start();
         consumer.join();
         producer.join();
+        // Endtime, take diff.
     }
 
     
